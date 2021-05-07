344f067f147e7bdaa2724212261f925e0836984a
Alexsandr <alexnem@inbox.ru>
2021-05-07 16:05:28 +0300

    setting .env & morgan;

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { readdirSync } from 'fs';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.APP_PORT || 8000;
+
+app.use(morgan('dev'));
 
 readdirSync('./routes').map((r) =>
   app.use('/api', require(`./routes/${r}`)));
 
-app.listen(5000, () => console.log('Server running'))
+app.listen(port, () => console.log('Server running ', port))
